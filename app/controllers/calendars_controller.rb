d02be1878cd0b0c3e1811402072ba750f532a633
yoshihiro-toyomura <haoguanglicun50@gmail.com>
2025-08-14 21:39:54 +0900

    Update calendars_controller.rb

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -16,7 +16,7 @@
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def get_week
@@ -41,7 +41,7 @@
         wday_num = wday_num -7
       end
 
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wday => wdays[wday_num]
+        days = { :month = (@todays_date + x).month, :date = (@todays_date+x).day, :plans => today_plans, :wday => wdays[wday_num]}
       
 
       @week_days.push(days)
